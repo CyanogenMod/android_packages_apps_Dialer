62a4e2cd31175b1f6e93efc21dec8461fadfb8f6
Adnan <adnan@cyngn.com>
2014-07-10 16:44:30 -0700

    Dialer: Introduce usage pattern stats.
    Change-Id: I9ca075c2726867d693c1f16700abe6f6aa276445

---
+++ src/com/android/dialer/cmstats/DialerStats.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.dialer.cmstats;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+public class DialerStats {
+    private static final String PREFS_FILE = "dialerstats";
+
+    public static final class Fields {
+        public static final String EVENT_CATEGORY = "category";
+        public static final String EVENT_ACTION = "action";
+        public static final String EVENT_LABEL = "label";
+        public static final String EVENT_VALUE = "value";
+    }
+
+    public static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(PREFS_FILE, 0);
+    }
+
+    public static void sendEvent(Context context, String category, String action,
+                                 String label, String value) {
+        // Create new intent
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(context, ReportingIntentService.class));
+
+        // append
+        intent.putExtra(Fields.EVENT_CATEGORY, category);
+        intent.putExtra(Fields.EVENT_ACTION, action);
+
+        // check if exist
+        if (label != null) {
+            intent.putExtra(Fields.EVENT_LABEL, label);
+        }
+
+        if (value != null) {
+            intent.putExtra(Fields.EVENT_VALUE, value);
+        }
+
+        context.startService(intent);
+    }
+
+    public static void sendEvent(Context context, String category, String action) {
+        sendEvent(context, category, action, null, null);
+    }
+}
